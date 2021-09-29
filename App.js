ab3aea5f4cacc9d811e524ca5d594f99d7eca2b5
Diego Barbosa <diegobarbosamorim@gmail.com>
2021-09-29 17:39:12 -0300

    Fixing layout

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import Routes from './src/routes';
@@ -8,6 +8,7 @@ import Routes from './src/routes';
 export default function App() {
   return (
     <NavigationContainer>
+      <StatusBar hidden/>
       <Routes />
     </NavigationContainer>
   );
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import {View, Text} from 'react-native';
+
+import {Feather} from '@expo/vector-icons';
+
+import Header from '../../components/Header'
+import {Container, SearchContainer, Input, SearchButton} from './styles';
+
 
 function Home() {
     return(
-        <>
-        <Text>Tela</Text>
-        </>
+        <Container>
+            <Header title="React Prime"/>
+            <SearchContainer>
+                <Input 
+                placeholder="Vingadores"
+                placeholderTextColor="#ddd"
+                />
+                <SearchButton>
+                    <Feather name="search" size={30} color="#FFF"/>
+                </SearchButton>
+            </SearchContainer>
+        </Container>
     )
 }
 
