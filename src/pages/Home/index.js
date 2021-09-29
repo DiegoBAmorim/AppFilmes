7dff81eec3c2a22579494efe8ac36f9d529c9995
Diego Barbosa <diegobarbosamorim@gmail.com>
2021-09-29 17:07:46 -0300

    Initial Commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native';
+
+function Home() {
+    return(
+        <>
+        <Text>Tela</Text>
+        </>
+    )
+}
+
+export default Home;
